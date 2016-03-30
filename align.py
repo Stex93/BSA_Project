84e997578320800dc339f1c232902aec3501bc88
Jouko <jouko.niinimaki@cs.helsinki.fi>
2016-03-30 16:31:46 +0300

    first release

--- align.py
+++ align.py
@@ -1,2 +1,72 @@
 
 
+vcf = open("calls.vcf", "r")
+
+line = "#"
+while(line[0] == "#"):
+    line = vcf.readline()
+print line
+
+ref_original = ""
+ref_name = ""
+
+with open("A.fasta", "r") as f:
+    ref_name = f.readline().rstrip("\n")
+    for x in f:
+        ref_original += x.rstrip("\n")
+
+f.close()
+
+ref = []
+target = []
+l = len(ref_original)
+
+for i in range(l):
+    tmp = line.split("\t")
+    if i == int(tmp[1]):
+        ref_frag = tmp[3]
+        target_frag = tmp[4].split(",")[0]  #fix maybe later
+
+        print ref_frag
+        print target_frag
+        print "----------"
+
+        if len(ref_frag) == len(target_frag):
+
+            #ref.append("--")
+            ref.append(ref_frag)
+            #ref.append("--")
+            target.append(target_frag)
+        elif len(ref_frag) > len(target_frag):
+            ref.append(ref_frag)
+            target.append(target_frag)
+            for a in range(len(ref_frag) - len(target_frag)):
+                target.append("-")
+        elif len(ref_frag) < len(target_frag):
+            ref.append(ref_frag)
+            target.append(target_frag)
+            for a in range(len(target_frag) - len(ref_frag)):
+                ref.append("-")
+
+        i += len(ref_frag) - 1
+
+        nextline = vcf.readline()
+        if len(nextline) > 1:
+            line = nextline
+    else:
+        ref.append(ref_original[i])
+        target.append(ref_original[i])
+
+vcf.close()
+
+ref_str = "".join(ref)
+target_str = "".join(target)
+
+output = open("output.fasta", "w")
+
+output.write(ref_name + "\n")
+output.write(ref_str + "\n")
+output.write(">B'\n")
+output.write(target_str + "\n")
+
+output.close()
