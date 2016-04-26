fb3a2e129d301a0a14f675fd0defc0de65069c1b
Jouko <jouko.niinimaki@cs.helsinki.fi>
2016-04-26 17:26:05 +0300

    large indels early version

--- align2.py
+++ align2.py
@@ -16,6 +16,13 @@ if __name__ == '__main__':
         print "Exiting.."
         sys.exit()
 
+    try:
+        vcf_large = open("./" + sys.argv[3], "r")
+    except IOError:
+        print "No such file or directory: " + sys.argv[3]
+        print "Exiting.."
+        sys.exit()
+
     print "Calculating final alignment.."
 
     line = "#"
@@ -44,12 +51,12 @@ if __name__ == '__main__':
         pos = int(tmp[1])
         ref_frag = tmp[3]
         target_frag = tmp[4].split(",")[0]  # fix maybe later
-        if len(ref_frag) == len(target_frag):
+        if len(ref_frag) == len(target_frag):   #SNP
             i = 0
             for c in target_frag:
                 target[pos - 1 + i + insertions] = c
                 i += 1
-        elif len(ref_frag) > len(target_frag):
+        elif len(ref_frag) > len(target_frag):  #deletion
             i = 0
             for c in target_frag:
                 target[pos - 1 + i + insertions] = c
@@ -58,7 +65,7 @@ if __name__ == '__main__':
             for j in range(n):
                 target[pos - 1 + i + insertions] = "-"
                 i += 1
-        elif len(ref_frag) < len(target_frag):
+        elif len(ref_frag) < len(target_frag):  #insertion
             i = 0
             for c in target_frag[:len(ref_frag)]:
                 target[pos - 1 + i + insertions] = c
@@ -72,6 +79,59 @@ if __name__ == '__main__':
         line = vcf.readline()
 
     vcf.close()
+
+    #calculating offsets of the index of ref
+    offset = []
+    off = 0
+    for c in ref:
+        if c == "-":
+            off += 1
+        offset.append(off)
+
+    line = "#"
+    while line[0] == "#":
+        line = vcf_large.readline()
+
+    insertions = 0
+    ooffset = 0
+    while len(line) > 1:
+        tmp = line.split("\t")
+        pos = int(tmp[1])
+        ref_frag = tmp[3]
+        target_frag = tmp[4].split(",")[0]  # fix maybe later
+
+        if len(ref_frag) > len(target_frag):    #deletion
+            i = 0
+            ooffset_new = ooffset
+            index_s = pos-1 + offset[pos-1] - ooffset + insertions
+            for a in range(index_s, index_s + len(ref_frag)):
+                if ref[a] == "-":
+                    ref.pop(a)
+                    target.pop(a)
+                    ooffset_new += 1
+
+            for c in target_frag:
+                target[pos - 1 + i + insertions + offset[pos - 1] - ooffset] = c
+                i += 1
+            n = len(ref_frag) - len(target_frag)
+            for j in range(n):
+                target[pos - 1 + i + insertions + offset[pos - 1] - ooffset] = "-"
+                i += 1
+        elif len(ref_frag) < len(target_frag):  #insertion
+            i = 0
+            for c in target_frag[:len(ref_frag)]:
+                target[pos - 1 + i + insertions + offset[pos - 1] - ooffset] = c
+                i += 1
+            n = len(target_frag) - len(ref_frag)
+            for j in range(n):
+                ref.insert(pos - 1 + i + insertions + offset[pos - 1] - ooffset, "-")
+                target.insert(pos - 1 + i + insertions + offset[pos - 1] - ooffset, target_frag[i])
+                insertions += 1
+                i += 1
+        line = vcf_large.readline()
+        ooffset = ooffset_new
+
+    vcf_large.close()
 
     ref_str = "".join(ref)
     target_str = "".join(target)
